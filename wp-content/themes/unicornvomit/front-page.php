47b8bf2e7ab038f4c5ec36fe9896250e17acea1b
ClaudiaNijholt <90260665+ClaudiaNijholt@users.noreply.github.com>
2022-05-11 15:57:57 +0200

    initial commit

---
+++ wp-content/themes/unicornvomit/front-page.php
@@ -0,0 +1,25 @@
+<?php
+
+/** config for home template **/
+
+$url = get_post_type_archive_link('sport');
+var_dump($url);
+exit();
+?>
+
+
+<div>
+    <nav>
+        <ul>
+            <li><a href="./wp-content/themes/unicornvomit/wp-dough-main/index.php">Home</a></li>
+            <img src="./wp-content/themes/unicornvomit/wp-dough-main/assets/img/logo_small_icon_only_inverted.png" alt="">
+            <li><a href="./wp-content/themes/unicornvomit/wp-dough-main/template-overzicht.php">Overzichtspagina</a></li>
+        </ul>
+    </nav>
+</div>
+<div class="banner">
+    <img src="./wp-content/themes/unicornvomit/wp-dough-main/assets/img/logo_large.png" alt="">
+</div>
+<h3 class="slowgun">Wereld vol wonderen</h3>
+<br>
+<h1 class="h1-homepage">Laatste reviews</h1>
